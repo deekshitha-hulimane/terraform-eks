d543713c4a2a277d0eb234526b15f2a881a5c26f
deekshitha-hulimane <deekshithahs9@gmail.com>
2026-04-17 15:44:53 +0530

    Update nodes.tf

--- main/nodes.tf
+++ main/nodes.tf
@@ -2,7 +2,9 @@ resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "my-worker-nodes"
   node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  
+  # FIX: Use the Public Subnet IDs so nodes can reach the EKS API
+  subnet_ids      = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
   scaling_config {
     desired_size = 2
@@ -10,9 +12,9 @@ resource "aws_eks_node_group" "nodes" {
     min_size     = 1
   }
 
-  # CHANGE THIS LINE
-  instance_types = ["t3.micro"] 
+  instance_types = ["t3.micro"]
 
+  # These ensure the IAM roles are ready before the nodes start
   depends_on = [
     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
