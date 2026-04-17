dec2181f5b7f03c3daf4be3f3a06d8f474d37b3c
deekshitha-hulimane <deekshithahs9@gmail.com>
2026-04-17 14:11:22 +0530

    Create nodes.tf

---
+++ main/nodes.tf
@@ -0,0 +1,22 @@
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "my-worker-nodes"
+  node_role_arn   = aws_iam_role.node_role.arn
+  
+  # Ensure these match your private subnets from vpc.tf
+  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
