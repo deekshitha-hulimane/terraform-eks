31692d9b4bdd330f901137ab0a25a346360fa611
deekshitha-hulimane <deekshithahs9@gmail.com>
2026-04-17 10:40:48 +0530

    Update vpc.tf

--- main/vpc.tf
+++ main/vpc.tf
@@ -12,6 +12,11 @@ resource "aws_subnet" "public_1" {
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
+tags = {
+  "Name" = "public-subnet-1"
+  "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  "kubernetes.io/role/elb" = "1"
+}
 }
 
 resource "aws_subnet" "public_2" {
@@ -19,6 +24,11 @@ resource "aws_subnet" "public_2" {
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
   map_public_ip_on_launch = true
+tags = {
+  "Name" = "public-subnet-2"
+  "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  "kubernetes.io/role/elb" = "1"
+}
 }
 
 # Private Subnets
@@ -26,10 +36,20 @@ resource "aws_subnet" "private_1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "ap-south-1a"
+tags = {
+  "Name" = "private-subnet-1"
+  "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  "kubernetes.io/role/internal-elb" = "1"
+}
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.4.0/24"
   availability_zone = "ap-south-1b"
+tags = {
+  "Name" = "private-subnet-2"
+  "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  "kubernetes.io/role/internal-elb" = "1"
+}
 }
