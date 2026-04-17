70c1d4251bac65bbfb29270baf79ed0f9c72653f
deekshitha-hulimane <deekshithahs9@gmail.com>
2026-04-17 09:27:29 +0530

    Update backend.tf

--- main/backend.tf
+++ main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-bucket-unique-12345"
+    bucket = "my-tf-state-bucket-unique-67890"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
